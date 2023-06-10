afc2443d66c86a523dc986c4c1f97f418e62a754
oliver58 <jolliver1971@gmail.com>
2023-06-10 19:37:19 +0800

    add basic request for the server

--- src/server.js
+++ src/server.js
@@ -2,4 +2,9 @@ import express from "express";
 
 const app = express();
 
+app.use((req, res) => {
+  console.log("The server has received a request!");
+  res.send("Hello from Express!");
+});
+
 app.listen(8080, () => console.log("Server is listening on port 8080..."));
